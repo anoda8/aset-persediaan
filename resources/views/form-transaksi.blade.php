996b26babbbb83a8947901aff5f7d4f6f85f3f99
anoda8 <nosada29@gmail.com>
2025-11-16 23:42:16 +0700

    fluxux

--- resources/views/form-transaksi.blade.php
+++ resources/views/form-transaksi.blade.php
@@ -9,7 +9,12 @@
                 </h2>
             </div>
         </div>
-        <flux:input wire:model="nama_penerima" label="Nama Penerima" class:input="font-mono" />
+        <flux:input.group label="Nama Penerima">
+            <flux:input wire:model="nama_penerima" class:input="font-mono" />
+            <flux:modal.trigger name="cari-penerima">
+            <flux:button icon="plus" class="cursor-pointer">New post</flux:button>
+            </flux:modal.trigger>
+        </flux:input.group>
         <flux:input wire:model="keperluan" label="Keperluan" class:input="font-mono" />
         <flux:input type="date" wire:model="tanggal" label="Tanggal" class:input="font-mono" />
 
@@ -18,6 +23,21 @@
         </div>
         <p>&nbsp;</p>
     </form>
+
+    <flux:modal name="cari-penerima" class="md:w-96">
+        <div class="space-y-6">
+            <div>
+                <flux:heading size="lg">Update profile</flux:heading>
+                <flux:text class="mt-2">Make changes to your personal details.</flux:text>
+            </div>
+            <flux:input label="Name" placeholder="Your name" />
+            <flux:input label="Date of birth" type="date" />
+            <div class="flex">
+                <flux:spacer />
+                <flux:button type="submit" variant="primary">Save changes</flux:button>
+            </div>
+        </div>
+    </flux:modal>
     @if (session('message'))
         <div class="mt-4 text-green-600">
             {{ session('message') }}
